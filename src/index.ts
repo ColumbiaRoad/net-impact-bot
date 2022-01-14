049c7c345175eae522f6270265759024cf5a18f3
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2022-01-14 14:25:23 +0200

    Add a basic test

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,3 @@
-"use strict";
+import { start } from "./server";
 
-import Hapi from "@hapi/hapi";
-
-const init = async () => {
-  const server = Hapi.server({
-    port: 3000,
-    host: "localhost",
-  });
-
-  server.route({
-    method: "GET",
-    path: "/",
-    handler: (_request, _h) => {
-      return "Hello World!";
-    },
-  });
-
-  await server.start();
-  console.log("Server running on %s", server.info.uri);
-};
-
-process.on("unhandledRejection", (err) => {
-  console.log(err);
-  process.exit(1);
-});
-
-init();
+start();
---
+++ tests/hello.test.ts
@@ -0,0 +1,24 @@
+import * as Hapi from "@hapi/hapi";
+
+import { init } from "../src/server";
+
+describe("Hello World", () => {
+  let server: Hapi.Server;
+
+  beforeAll(async () => {
+    server = await init();
+  });
+
+  afterAll(async () => {
+    await server.stop();
+  });
+
+  test("Hello World endpoint works", async () => {
+    const res = await server.inject({
+      method: "GET",
+      url: "/",
+    });
+    expect(res.statusCode).toEqual(200);
+    expect(res.payload).toEqual("Hello World!");
+  });
+});
