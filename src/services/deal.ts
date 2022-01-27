207c4559875153092e12dadf7656b224c0d848dd
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2022-01-27 16:20:37 +0200

    Log steps

--- src/services/deal.ts
+++ src/services/deal.ts
@@ -18,14 +18,18 @@ const postDeal = async (request: Hapi.Request, _h: Hapi.ResponseToolkit) => {
 };
 
 const dealPipeline = async (objectId: number) => {
+  console.log("deal received");
   const companyIds = await getDealCompanies(objectId);
-
+  console.log("deal company id(s) deducted");
   for (let i = 0; i < companyIds.length; i++) {
     const company = await getCompany(companyIds[i]);
+    console.log("company information fetched");
     const uprightId = getUprightId(company);
     if (uprightId) {
       const profile = await getProfile(uprightId);
+      console.log("upright profile received");
       await uploadImage(profile, company.name);
+      console.log("upright profile posted to slack");
     }
   }
 };
