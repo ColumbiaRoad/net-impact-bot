9c3ea7a3d9142b8d1690a0c65664fb04e92bcb48
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2022-02-02 16:34:08 +0200

    Catch errors

--- src/services/slack.ts
+++ src/services/slack.ts
@@ -5,12 +5,18 @@ const web = new WebClient(config.slackToken);
 
 const uploadImage = async (img: Buffer, companyName: string) => {
   const message = `The Net Impact Profile for ${companyName} as a company. What is the impact of our work with them? See thread.`;
-  await web.files.upload({
-    channels: config.slackChannel,
-    initial_comment: message,
-    file: img,
-    filename: companyName,
-  });
+  try {
+    await web.files.upload({
+      channels: config.slackChannel,
+      initial_comment: message,
+      file: img,
+      filename: companyName,
+    });
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+  return true;
 };
 
 export { uploadImage };
