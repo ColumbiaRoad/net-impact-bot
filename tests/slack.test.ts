b4ddc0440a748910913e48f57eb9110afdc8975d
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2022-02-03 22:40:20 +0200

    Test slack

---
+++ tests/slack.test.ts
@@ -0,0 +1,44 @@
+import { WebClient } from "@slack/web-api";
+import { Buffer } from "buffer";
+import { uploadImage } from "../src/services/slack";
+import config from "../src/config";
+
+jest.mock("@slack/web-api", () => {
+  const mSlack = {
+    files: {
+      upload: jest.fn(),
+    },
+  };
+  return { WebClient: jest.fn(() => mSlack) };
+});
+
+describe("Upload to Slack", () => {
+  const buff = Buffer.from("test");
+  const name = "Test Company";
+  let slack: WebClient;
+
+  beforeAll(() => {
+    slack = new WebClient();
+  });
+
+  test("returns true if worked", async () => {
+    const posted = await uploadImage(buff, name);
+    expect(slack.files.upload).toBeCalledWith({
+      channels: config.slackChannel,
+      initial_comment: `The Net Impact Profile for ${name} as a company. What is the impact of our work with them? See thread.`,
+      file: buff,
+      filename: name,
+    });
+    expect(posted).toBe(true);
+  });
+
+  test("returns false if didn't work", async () => {
+    const uploadMock = slack.files.upload as jest.MockedFunction<
+      typeof slack.files.upload
+    >;
+    uploadMock.mockRejectedValue(() => new Error("slack error"));
+    const posted = await uploadImage(buff, name);
+    expect(slack.files.upload).toBeCalled();
+    expect(posted).toBe(false);
+  });
+});
