ee2fdc201c364dd9f92c30e7b3401a0adb49b801
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2022-08-02 16:39:04 +0300

    enable Redis

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,28 @@
 "use strict";
 
 import * as Hapi from "@hapi/hapi";
+import * as CatboxRedis from "@hapi/catbox-redis";
 import "dotenv/config";
 import dealRoutes from "./routes/deals";
 
 const server = Hapi.server({
   port: process.env.PORT || 3000,
   host: process.env.HOST || "0.0.0.0",
+  cache: [
+    {
+      name: "redis",
+      provider: {
+        constructor: CatboxRedis,
+      },
+    },
+  ],
 });
 
 // Register plugins
 const registerPlugins = async () => {
   // Routes
   await server.register([dealRoutes]);
+  // Server methods
 };
 
 server.route({
@@ -20,6 +30,14 @@ server.route({
   path: "/",
   handler: (_request, _h) => {
     return "Hello World2";
+  },
+});
+
+server.route({
+  method: "GET",
+  path: "/example",
+  handler: () => {
+    return server.methods.example();
   },
 });
 
