50ef711fce76512513dbe0d77431f701c765557a
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2022-08-03 20:33:55 +0300

    Register example server method with caching

--- src/methods/example.ts
+++ src/methods/example.ts
@@ -1,6 +1,27 @@
 "use strict";
 
-import Hapi from "@hapi/hapi";
+import { ServerMethod } from "@hapi/hapi";
+import { PolicyOptions } from "@hapi/catbox";
+
+/*
+  For some reason, @types/hapi__catbox is missing the property `cache` from
+  `ServerMethodCache` so this needs to be added manually. Create a PR if
+  you know a more elegant solution.
+*/
+interface CustomServerMethodCache extends PolicyOptions<unknown> {
+  generateTimeout: number | false;
+  cache?: string;
+}
+interface CustomServerMethodOptions {
+  cache?: CustomServerMethodCache | undefined;
+}
+interface CustomServer {
+  method(
+    name: string,
+    method: ServerMethod,
+    options?: CustomServerMethodOptions
+  ): void;
+}
 
 const exampleFunc = () => {
   return new Date();
@@ -8,10 +29,11 @@ const exampleFunc = () => {
 
 const example = {
   name: "methods/example",
-  register: function (server: Hapi.Server) {
+  register: function (server: CustomServer) {
     server.method("example", exampleFunc, {
       cache: {
-        expiresIn: 10000,
+        cache: "redis",
+        expiresIn: 10 * 1000, // 10 s
         generateTimeout: 2000,
       },
     });
