90cb0884c1839c0a3f516c35029e05a9e16b0248
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2022-08-06 21:01:43 +0300

    Add `uprightInternalGet` server method

--- src/config.ts
+++ src/config.ts
@@ -11,6 +11,8 @@ const envSchema = Joi.object({
 const env = {
   hsAccessToken: process.env.HUBSPOT_ACCESS_TOKEN,
   uprightApiToken: process.env.UPRIGHT_API_TOKEN,
+  uprightUserEmail: process.env.UPRIGHT_USER_EMAIL,
+  uprightUserPassword: process.env.UPRIGHT_USER_PASSWORD,
   slackToken: process.env.SLACK_TOKEN,
   slackChannel: process.env.SLACK_CHANNEL,
   slackErrorChannel: process.env.SLACK_ERROR_CHANNEL || null,
@@ -22,4 +24,5 @@ export default {
   ...env,
   hsApiRoot: "https://api.hubapi.com",
   uprightApiRoot: "https://api.uprightproject.com/v1",
+  uprightInternalApiRoot: "https://uprightplatform.com/api",
 };
