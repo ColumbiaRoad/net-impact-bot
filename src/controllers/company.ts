62574949890cde488eb8efb69842bd725334d773
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2022-09-21 10:52:39 +0300

    Reorder logging

--- src/controllers/company.ts
+++ src/controllers/company.ts
@@ -39,11 +39,12 @@ const handleUpdateUid = async (
   const helper = request.payload as { payload: string };
   console.log("helper", helper);
   console.log("helper.payload", helper.payload);
+  console.log("typeof helper.payload", typeof helper.payload);
   console.log(
     "decodeURIComponent(helper.payload)",
     decodeURIComponent(helper.payload)
   );
-  console.log("typeof helper.payload", typeof helper.payload);
+
   console.log(
     "JSON.parse(decodeURIComponent(helper.payload))",
     JSON.parse(decodeURIComponent(helper.payload))
