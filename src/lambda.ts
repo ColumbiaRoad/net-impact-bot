4dc986dee0248c11237b78e4d19a5a32f3b9ab58
Max Mäkipää <max.makipaa@columbiaroad.com>
2025-06-18 15:10:57 +0300

    fixed the lambda handler, deployed aws lambda now working

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,52 +1,23 @@
 import {
   APIGatewayProxyEvent,
   APIGatewayProxyResult,
+  Context,
+  Callback,
   Handler,
 } from "aws-lambda";
-import { IncomingMessage, ServerResponse } from "http";
-import { init } from "./server";
 import serverlessExpress from "@codegenie/serverless-express";
-import type { Server } from "@hapi/hapi";
+import appPromise from "./app";
 
-type HapiRequestListener = (
-  req: IncomingMessage,
-  res: ServerResponse<IncomingMessage>
-) => void;
-
-// Our cached Lambda handler
 let cachedHandler: Handler<APIGatewayProxyEvent, APIGatewayProxyResult>;
 
-/**
- * Lambda entry point.
- */
-export const handler: Handler<APIGatewayProxyEvent, APIGatewayProxyResult> = (
-  event,
-  context,
-  callback
+export const handler: Handler = async (
+  event: APIGatewayProxyEvent,
+  context: Context,
+  callback: Callback
 ) => {
-  // Allow Lambda to return as soon as the HTTP response is sent
-  context.callbackWaitsForEmptyEventLoop = false;
-
-  // Cold start: initialize Hapi → extract its `request` listener → wrap it
   if (!cachedHandler) {
-    init()
-      .then((hapiServer: Server) => {
-        const listener = (
-          hapiServer.listener as unknown as {
-            _events: { request: HapiRequestListener };
-          }
-        )._events.request;
-
-        cachedHandler = serverlessExpress({ app: listener });
-        cachedHandler(event, context, callback);
-      })
-      .catch((err) => {
-        // If init() fails, surface it to API Gateway
-        callback(err as Error);
-      });
+    const app = await appPromise;
+    cachedHandler = serverlessExpress({ app });
   }
-  // Warm start: just delegate
-  else {
-    cachedHandler(event, context, callback);
-  }
+  return cachedHandler(event, context, callback);
 };
